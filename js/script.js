f7d9e9a556cdfa6f772f2b0d28ac31feefc84859
Alessandro <alessandro.corneli11@mail.com>
2020-01-08 17:41:47 +0100

    2

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 var elenco = ["Rossi","Bianchi","Verdi","Orsini"];
 var cognomeUser = prompt("Inserisci il tuo cognome:");
 elenco.push(cognomeUser);
+for (var i = 0; i < elenco.length; i++) {
+  elenco[i] = elenco[i].toUpperCase();
+}
 elenco.sort();
 for ( i = 0; i < elenco.length; i++) {
   document.getElementById('listaUsers').innerHTML += '<li>' + elenco[i] + '</li>';
 }
-console.log("Elenco partecipanti:<p>");
-console.log(elenco);
+document.writeln("Elenco partecipanti:<p>");
+document.writeln(elenco);
